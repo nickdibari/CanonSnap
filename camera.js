d77f6f943673cb527ca2b918a41b6f409b290b34
nickdibari <ndibari@fordham.edu>
2017-12-02 16:52:55 -0500

    Added logic to pull Human metadata from frame

--- camera.js
+++ camera.js
@@ -28,5 +28,40 @@ function onFrame (frame) {
     var height = frame.height; 
     var horizontal = frame.angle.horizontal; 
     var tilt = frame.angle.tilt; 
-    var type = frame.description.type
+    var type = frame.detection.type;
+
+    // Find Human Subjects in frame
+    if (type === 'Human'){
+        console.log('Got a live one!');
+        var subjects = frame.detection.subjects;
+        var len = frame.detection.subjects.length;
+        let subjectList = [];
+
+        for (let i = 0; i < len; i++){
+            var human = {};
+            var id = subjects[i].subjectID;
+            var cenX = subjects[i].centerX;
+            var cenY = subjects[i].centerY;
+            var leftEyeX = subjects[i].leftEyeX;
+            var leftEyeY = subjects[i].leftEyeY;
+            var rightEyeX = subjects[i].rightEyeX;
+            var rightEyeY = subjects[i].rightEyeY;
+            human = {
+                id: id,
+                cenX: cenX,
+                cenY: cenY,
+                leftEyeX: leftEyeX,
+                leftEyeY: leftEyeY,
+                rightEyeX: rightEyeX,
+                rightEyeY: rightEyeY,
+            };
+            console.log('Properly parsed Human: ' + id);
+            subjectList.push(human)
+            console.log(subjectList)
+        }
+    }
+
+    else{
+        console.log('No humans here..')
+    }
 }

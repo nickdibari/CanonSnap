358ccc62c293bd901c6e5b50139c33067aa42f09
jlee124 <jessica.lee124@gmail.com>
2017-12-03 09:44:05 -0500

    new version

--- webapp/views.py
+++ webapp/views.py
@@ -6,6 +6,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+def index(request):
+    return render(request, 'webapp/index.html')
+
+"""
 from .models import Question
 
 def index(request):
@@ -13,13 +17,12 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
-    """ Harder way
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))"""
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     try:
@@ -27,10 +30,10 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
-    
+
 def results(request, question_id):
     response = "You're looking at the results of question %s."
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return HttpResponse("You're voting on question %s." % question_id)"""
